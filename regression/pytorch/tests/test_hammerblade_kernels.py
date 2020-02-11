a25cbb2ad021f28fe2de0baaafaa0afc8abc66ac
lc873 <lc873@cornell.edu>
2020-02-11 14:17:37 -0500

    [sub] sub tests

--- regression/pytorch/tests/test_hammerblade_kernels.py
+++ regression/pytorch/tests/test_hammerblade_kernels.py
@@ -53,3 +53,35 @@ def test_elementwise_add_4():
   assert y_h.device == torch.device("hammerblade")
   assert torch.equal(y_c, y)
 
+def test_elementwise_sub_1():
+  x = torch.ones(1,10)
+  y = torch.ones(1,10)
+  z = x - y
+  z_h = x.hammerblade() - y.hammerblade()
+  assert z_h.device == torch.device("hammerblade")
+  assert torch.equal(z_h.cpu(), z)
+
+def test_elementwise_sub_2():
+  x = torch.ones(4,5)
+  y = torch.ones(4,5)
+  z = x - y
+  z_h = x.hammerblade() - y.hammerblade()
+  assert z_h.device == torch.device("hammerblade")
+  assert torch.equal(z_h.cpu(), z)
+
+def test_elementwise_sub_3():
+  x = torch.rand(1,128)
+  y = torch.rand(1,128)
+  z = x - y
+  z_h = x.hammerblade() - y.hammerblade()
+  assert z_h.device == torch.device("hammerblade")
+  assert torch.equal(z_h.cpu(), z)
+
+def test_elementwise_sub_4():
+  x = torch.rand(16,32)
+  y = torch.rand(16,32)
+  z = x - y
+  z_h = x.hammerblade() - y.hammerblade()
+  assert z_h.device == torch.device("hammerblade")
+  assert torch.equal(z_h.cpu(), z)
+
