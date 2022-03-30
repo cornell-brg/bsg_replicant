690e04fa49aa55757ef4ef02b5acf4e85bc54fdf
lc873 <lc873@cornell.edu>
2022-03-30 16:13:51 -0400

    [ligra-bfs] removing unused files

--- examples/cuda/appl-bfs/parallel.h
+++ examples/cuda/appl-bfs/parallel.h
@@ -24,6 +24,8 @@
 #ifndef _PARALLEL_H
 #define _PARALLEL_H
 
+#include <cstdint>
+
 static int getWorkers()
 {
   return 1;
@@ -32,29 +34,14 @@ static void setWorkers( int n ) {}
 
 #include <limits.h>
 
-#if defined( LONG )
-typedef long          intT;
-typedef unsigned long uintT;
-#define INT_T_MAX LONG_MAX
-#define UINT_T_MAX ULONG_MAX
-#else
-typedef int          intT;
-typedef unsigned int uintT;
+typedef int32_t  intT;
+typedef uint32_t uintT;
 #define INT_T_MAX INT_MAX
 #define UINT_T_MAX UINT_MAX
-#endif
 
-// edges store 32-bit quantities unless EDGELONG is defined
-#if defined( EDGELONG )
-typedef long          intE;
-typedef unsigned long uintE;
-#define INT_E_MAX LONG_MAX
-#define UINT_E_MAX ULONG_MAX
-#else
-typedef int          intE;
-typedef unsigned int uintE;
+typedef int32_t  intE;
+typedef uint32_t uintE;
 #define INT_E_MAX INT_MAX
 #define UINT_E_MAX UINT_MAX
-#endif
 
 #endif // _PARALLEL_H
