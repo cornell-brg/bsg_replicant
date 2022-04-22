11d2724babedac137980e816869bd56957da147f
Max Ruttenberg <mrutt@cs.washington.edu>
2022-04-22 15:43:21 -0700

    revised nqueens with parameter sweeping

---
+++ examples/cuda/appl-nqueens-rev/tests.mk
@@ -0,0 +1,10 @@
+# 2x2 tests
+TESTS += $(call test-name,1,1,2,2)
+TESTS += $(call test-name,2,1,2,2)
+TESTS += $(call test-name,3,1,2,2)
+# TESTS += $(call test-name,4,1,2,2)
+# # 16x8 tests
+# TESTS += $(call test-name,1,1,16,8)
+# TESTS += $(call test-name,2,1,16,8)
+# TESTS += $(call test-name,3,1,16,8)
+# TESTS += $(call test-name,4,1,16,8)
