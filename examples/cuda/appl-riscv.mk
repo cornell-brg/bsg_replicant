73b19bd1da03c1818d683af870aae3e9cb6a3021
Max Ruttenberg <mrutt@cs.washington.edu>
2022-04-22 15:38:26 -0700

    [appl] adds makefile for appl in replicant

---
+++ examples/cuda/appl-riscv.mk
@@ -0,0 +1,38 @@
+_REPO_ROOT ?= $(shell git rev-parse --show-toplevel)
+-include $(_REPO_ROOT)/environment.mk
+-include $(BSG_MACHINE_PATH)/Makefile.machine.include
+
+RISCV_CXXFLAGS += -I$(BSG_MANYCORE_DIR)/software/spmd/applrts/
+RISCV_CXXFLAGS += -I$(BSG_MANYCORE_DIR)/software/spmd/appl/
+
+vpath %.cpp $(BSG_MANYCORE_DIR)/software/spmd/appl
+vpath %.c   $(BSG_MANYCORE_DIR)/software/spmd/appl
+vpath %.S   $(BSG_MANYCORE_DIR)/software/spmd/appl
+vpath %.cpp $(BSG_MANYCORE_DIR)/software/spmd/applrts
+vpath %.c   $(BSG_MANYCORE_DIR)/software/spmd/applrts
+vpath %.S   $(BSG_MANYCORE_DIR)/software/spmd/applrts
+
+# APPL implemenation
+ifeq ($(APPL_IMPL), APPL_IMPL_APPLRTS)
+	RISCV_CXXFLAGS  +=-DAPPL_IMPL_APPLRTS
+	RISCV_TARGET_OBJECTS += appl-runtime.o
+	RISCV_TARGET_OBJECTS += applrts-config.o
+	RISCV_TARGET_OBJECTS += applrts-runtime.o
+	RISCV_TARGET_OBJECTS += applrts-scheduler.o
+endif
+
+ifeq ($(APPL_IMPL), APPL_IMPL_SERIAL)
+	RISCV_CXXFLAGS += -DAPPL_IMPL_SERIAL
+endif
+
+ifeq ($(APPL_IMPL), APPL_IMPL_CELLO)
+	CELLO_DIR := $(CL_DIR)/../cello
+	RISCV_CXXFLAGS +=-DAPPL_IMPL_CELLO
+	RISCV_CXXFLAGS +=-I$(CELLO_DIR)/include
+	RISCV_CXXFLAGS +=-I$(CELLO_DIR)/arch/hammerblade/include
+	RISCV_CXXFLAGS += -fno-threadsafe-statics
+	RISCV_TARGET_OBJECTS += appl-runtime.o
+endif
+
+# include riscv builddefs
+include $(_REPO_ROOT)/examples/cuda/riscv.mk
