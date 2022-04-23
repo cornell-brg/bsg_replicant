5be2b6627531de68fc98e7a64935f9264df67bf5
Max Ruttenberg <mrutt@cs.washington.edu>
2022-04-23 11:22:41 -0700

    [appl] parameterized knapsack

---
+++ examples/cuda/appl-knapsack-rev/tests.mk
@@ -0,0 +1,48 @@
+#############
+# example 1 #
+#############
+# 2x2
+TESTS += $(call test-name,knapsack-example1.input,2,2,APPLRTS)
+TESTS += $(call test-name,knapsack-example1.input,2,2,CELLO)
+TESTS += $(call test-name,knapsack-example1.input,2,2,SERIAL)
+# 16x8
+TESTS += $(call test-name,knapsack-example1.input,16,8,APPLRTS)
+TESTS += $(call test-name,knapsack-example1.input,16,8,CELLO)
+TESTS += $(call test-name,knapsack-example1.input,16,8,SERIAL)
+
+#############
+# example 2 #
+#############
+# 2x2
+TESTS += $(call test-name,knapsack-example2.input,2,2,APPLRTS)
+TESTS += $(call test-name,knapsack-example2.input,2,2,CELLO)
+TESTS += $(call test-name,knapsack-example2.input,2,2,SERIAL)
+# 16x8
+TESTS += $(call test-name,knapsack-example2.input,16,8,APPLRTS)
+TESTS += $(call test-name,knapsack-example2.input,16,8,CELLO)
+TESTS += $(call test-name,knapsack-example2.input,16,8,SERIAL)
+
+#############
+# example 3 #
+#############
+# 2x2
+TESTS += $(call test-name,knapsack-example3.input,2,2,APPLRTS)
+TESTS += $(call test-name,knapsack-example3.input,2,2,CELLO)
+TESTS += $(call test-name,knapsack-example3.input,2,2,SERIAL)
+# 16x8
+TESTS += $(call test-name,knapsack-example3.input,16,8,APPLRTS)
+TESTS += $(call test-name,knapsack-example3.input,16,8,CELLO)
+TESTS += $(call test-name,knapsack-example3.input,16,8,SERIAL)
+
+###########
+# small 1 #
+###########
+# 2x2
+TESTS += $(call test-name,knapsack-small-1.input,2,2,APPLRTS)
+TESTS += $(call test-name,knapsack-small-1.input,2,2,CELLO)
+TESTS += $(call test-name,knapsack-small-1.input,2,2,SERIAL)
+# 16x8
+TESTS += $(call test-name,knapsack-small-1.input,16,8,APPLRTS)
+TESTS += $(call test-name,knapsack-small-1.input,16,8,CELLO)
+TESTS += $(call test-name,knapsack-small-1.input,16,8,SERIAL)
+
