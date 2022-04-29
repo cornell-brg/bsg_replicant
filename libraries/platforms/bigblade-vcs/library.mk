30865dfd977355538aa0b648c4ce4eddab83a8d1
mrutt92 <mrutt@cs.washington.edu>
2022-04-29 11:50:04 -0700

    PC Histogram Executable (#783)
    * pc-histogram execution mode
    
    * vdefines and dependencies for verilator
    
    * [bigblade-verilator] fixup
    
    * undo
    
    * [pc_histogram] scoreboard tracker pkg

--- libraries/platforms/bigblade-vcs/library.mk
+++ libraries/platforms/bigblade-vcs/library.mk
@@ -35,6 +35,8 @@ PLATFORM_CXXSOURCES += $(LIBRARIES_PATH)/features/tracer/simulation/bsg_manycore
 PLATFORM_REGRESSION_CSOURCES += $(LIBRARIES_PATH)/platforms/bigblade-vcs/bsg_manycore_regression_platform.c
 
 include $(LIBRARIES_PATH)/features/dma/simulation/feature.mk
+include $(LIBRARIES_PATH)/features/tracer/simulation/feature.mk
+include $(LIBRARIES_PATH)/features/pc_histogram/simulation/feature.mk
 
 PLATFORM_OBJECTS += $(patsubst %cpp,%o,$(PLATFORM_CXXSOURCES))
 PLATFORM_OBJECTS += $(patsubst %c,%o,$(PLATFORM_CSOURCES))
