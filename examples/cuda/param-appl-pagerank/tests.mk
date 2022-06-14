299bded381fe8dbfa060dd1da7d91f3013d6e373
lc873 <lc873@cornell.edu>
2022-06-14 09:54:40 -0400

    [param-appl-pagerank] update default tests

--- examples/cuda/param-appl-pagerank/tests.mk
+++ examples/cuda/param-appl-pagerank/tests.mk
@@ -1,1 +1,4 @@
-TESTS += $(call test-name,g10k8,1,16,8,APPLRTS)
+TESTS += $(call test-name,g14k16,1,16,8,APPLRTS)
+TESTS += $(call test-name,g14k16,1,16,8,STATIC)
+TESTS += $(call test-name,g16k32,1,16,8,APPLRTS)
+TESTS += $(call test-name,g16k32,1,16,8,STATIC)
