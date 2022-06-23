cbab346e24cb0e4b2c17e473242c497d095eec03
Max Ruttenberg <mrutt@cs.washington.edu>
2022-06-23 09:56:59 -0700

    bfs test list update

--- examples/cuda/param-appl-bfs/tests.mk
+++ examples/cuda/param-appl-bfs/tests.mk
@@ -1,4 +1,16 @@
-TESTS += $(call test-name,u10k8,0,0,1,2,2,APPLRTS)
-TESTS += $(call test-name,u10k8,0,1,1,2,2,APPLRTS)
-TESTS += $(call test-name,u10k8,0,2,1,2,2,APPLRTS)
-TESTS += $(call test-name,u10k8,0,3,1,2,2,APPLRTS)
+# g16k32
+# select a node with median degree as the start
+# kron
+TESTS += $(call test-name,g16k32,55670,0,1,16,8,CELLO)
+TESTS += $(call test-name,g16k32,55670,1,1,16,8,CELLO)
+TESTS += $(call test-name,g16k32,55670,2,1,16,8,CELLO)
+TESTS += $(call test-name,g16k32,55670,3,1,16,8,CELLO)
+TESTS += $(call test-name,g16k32,55670,4,1,16,8,CELLO)
+TESTS += $(call test-name,g16k32,55670,5,1,16,8,CELLO)
+# u16k32
+# uniform
+TESTS += $(call test-name,u16k32,55733,0,1,16,8,CELLO)
+TESTS += $(call test-name,u16k32,55733,1,1,16,8,CELLO)
+TESTS += $(call test-name,u16k32,55733,2,1,16,8,CELLO)
+TESTS += $(call test-name,u16k32,55733,3,1,16,8,CELLO)
+TESTS += $(call test-name,u16k32,55733,4,1,16,8,CELLO)
