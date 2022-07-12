b5c188f5c37e2b75ab8df060ce8316b8969d80b8
lc873 <lc873@cornell.edu>
2022-07-12 08:39:04 -0400

    [barnes-hut] change test name

--- examples/cuda/param-appl-barnes/template.mk
+++ examples/cuda/param-appl-barnes/template.mk
@@ -72,7 +72,7 @@ CDEFINES += -Dbsg_tiles_X=$(TILE_GROUP_DIM_X) -Dbsg_tiles_Y=$(TILE_GROUP_DIM_Y)
 CXXDEFINES +=
 
 FLAGS     = -g -Wall -Wno-unused-function -Wno-unused-variable
-FLAGS    += -DNBODY=$(fib-in)
+FLAGS    += -DNBODY=$(nbody)
 CFLAGS   += -std=c99 $(FLAGS)
 CXXFLAGS += -std=c++11 $(FLAGS)
 
